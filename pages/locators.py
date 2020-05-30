d3d538e3ccf265a051ffd894eb59516fa9bb4cf4
Harry <57965433+burundukov@users.noreply.github.com>
2020-05-30 15:31:48 +0300

    Some bugs changed

--- pages/locators.py
+++ pages/locators.py
@@ -5,17 +5,12 @@ class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 class LoginPageLocators():
-    '''
-    REGISTER_EMAIL_FORM = (By.CSS_SELECTOR, "#id_registration-email")
-    REGISTER_PASSWORD_FORM = (By.CSS_SELECTOR, "#id_registration-password1")
-    REGISTER_REPLY_PASSWORD_FORM = (By.CSS_SELECTOR, "#id_registration-password2")
-
-    SIGN_IN_EMAIL_FORM = (By.CSS_SELECTOR, "#id_login-username")
-    SIGN_IN_PASSWORD_FORM = (By.CSS_SELECTOR, "#id_login-password")
-    '''
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 
 class ProductPageLocators():
-
     ADD_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, "div#content_inner div.basket-items")
+    BASKET_EMPTY = (By.CSS_SELECTOR, "div#content_inner > p")
